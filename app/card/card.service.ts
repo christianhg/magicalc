1fe6311b5982c6b98d13d08f8b2f9b0fe4be884a
Christian Grøngaard <christianhg@gmail.com>
2016-03-28 23:07:08 +0200

    Share Cards Observable to limit http requests

--- app/card/card.service.ts
+++ app/card/card.service.ts
@@ -8,21 +8,23 @@ import {Card} from './card';
 @Injectable()
 
 export class CardService {
+  private cardsObservable: Observable<Card[]>;
+
   constructor(
     private http: Http) { }
 
   public getCards(): Observable<Card[]> {
-    return this.http.get('http://mtgjson.com/json/AllCards.json')
-      .map((res: Response) => {
-        return _.values(res.json());
-      });
+    if(!this.cardsObservable) {
+      this.cardsObservable = this.http.get('http://mtgjson.com/json/AllCards.json')
+        .map((res: Response) => {
+          return _.values(res.json());
+        });
+    }
+    return this.cardsObservable;
   }
 
   public getCardWithLongestName(): Observable<Card> {
-    return this.http.get('http://mtgjson.com/json/AllCards.json')
-      .map((res: Response) => {
-        return _.values(res.json());
-      })
+    return this.getCards()
       .map((cards: Card[]) => {
         return _.max(cards, (card: Card) => {
           return card.name.length;
@@ -31,10 +33,7 @@ export class CardService {
   }
 
   public getCardWithMostPower(): Observable<Card> {
-    return this.http.get('http://mtgjson.com/json/AllCards.json')
-      .map((res: Response) => {
-        return _.values(res.json());
-      })
+    return this.getCards()
       .map((cards: Card[]) => {
         return _.max(cards, (card: Card) => {
           return card.power;
@@ -43,10 +42,7 @@ export class CardService {
   }
 
   public getCardsWithHighestCMC(): Observable<Card[]> {
-    return this.http.get('http://mtgjson.com/json/AllCards.json')
-      .map((res: Response) => {
-        return _.values(res.json())
-      })
+    return this.getCards()
       .map((cards: Card[]) => {
         const highestCMC: number = _.max(cards, (card: Card) => {
           return card.cmc;
