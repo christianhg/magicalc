ef9e5c902129b7e3e88874d7f6af73bd27a157cf
Christian Grøngaard <christianhg@gmail.com>
2016-03-30 18:11:03 +0200

    Update Card interface with new and optional properties

--- app/card/card.ts
+++ app/card/card.ts
@@ -1,9 +1,17 @@
 export interface Card {
   cmc: number;
+  colorIdentity: string[];
+  colors: string[];
+  imageName: string;
+  layout: string;
+  manaCost: string;
+  mciNumber?: string;
   name: string;
-  pmr: number;
-  power: string;
+  pmr?: number;
+  power?: string;
+  subtypes?: string[];
   text: string;
-  toughness: string;
+  toughness?: string;
   type: string;
+  types: string[];
 }
